e9effc9448cb7518e50f88b9332079fbb159b081
Kuljo Apps <kuljoapps@gmail.com>
2026-01-28 14:38:47 +0000

    Ok, teraz działa. Teraz ostatni krok - pozbądź się całkowicie strzałeczk

--- src/app/fill-the-gap/gap-sentences/page.tsx
+++ src/app/fill-the-gap/gap-sentences/page.tsx
@@ -98,7 +98,7 @@ function SentenceExercise({
                     disabled={!!answerStates.sentence1}
                 >
                     <SelectTrigger className={cn(
-                        "h-8 font-semibold w-fit",
+                        "h-8 font-semibold w-fit [&>svg]:hidden pr-2",
                         answerStates[key] === 'correct' && 'border-success text-success ring-2 ring-success/50',
                         answerStates[key] === 'incorrect' && 'border-destructive text-destructive ring-2 ring-destructive/50'
                     )}>
